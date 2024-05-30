2046aa1ff108e716eab95be24b9dff8f1efc1371
Diego <sdieg82@gmail.com>
2024-05-30 14:52:38 -0500

    actionsReducer

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -1,4 +1,5 @@
-import { Guitar } from "../types";
+import { db } from "../data/db";
+import { CartItem, Guitar } from "../types";
 
 export type CartActions=
     { type:'add-to-cart',payload:{item:Guitar}}|
@@ -6,3 +7,54 @@ export type CartActions=
     { type:'decrease-quantity',payload:{id:Guitar['id']}}|
     { type:'increase-quantity',payload:{id:Guitar['id']}}|
     { type:'clear-cart'}
+
+    export type CartState={
+        data:Guitar[],
+        cart:CartItem[]
+    }
+
+    export const initialState:CartState={
+        data:db,
+        cart:[]
+    }
+
+    export const cartReducer=(
+        state:CartState=initialState,
+        action:CartActions
+    )=>{
+
+        if (action.type==='add-to-cart') {
+            return{
+                ...state
+            }
+        }
+
+        if (action.type==='remove-cart') {
+            return{
+                ...state,
+            }
+            
+        }
+
+        if (action.type==='decrease-quantity') {
+            return{
+                ...state,
+            }
+            
+        }
+
+        if (action.type==='increase-quantity') {
+            return{
+                ...state,
+            }
+            
+        }
+
+        if (action.type==='clear-cart') {
+            return{
+                ...state,
+            }
+            
+        }
+        return state
+    }
